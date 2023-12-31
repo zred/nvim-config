c40c1da9f92f21fe942ae94fa8d49b7bb46e0ed5
zred <zachary.redmond@gmail.com>
2023-12-30 21:28:06 -0600

    added c cpp zig rust

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query" },
+  ensure_installed = { "javascript", "typescript", "c", "rust", "zig", "cpp", "lua", "vim", "vimdoc", "query" },
   sync_install = false,
   auto_install = true,
   highlight = {
